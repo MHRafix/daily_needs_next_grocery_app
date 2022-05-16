d4b85df5a2f8d0b079d744525ad671e9c1877bbd
MHRafix <fmhrafiz27@gmail.com>
2022-05-16 15:33:53 +0600

    cart table is done

--- src/components/cart_page/TableBody.js
+++ src/components/cart_page/TableBody.js
@@ -1,18 +1,41 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 export default function TableBody({ carted_products }) {
   const { thumbnail, title, prices } = carted_products;
+  const [qty, setQty] = useState(1);
+
   return (
     <div className="table_body">
       <div className="table_body_item">
-        <Image src={thumbnail} alt="carted items thumbnail" />
+        <Image
+          src={thumbnail}
+          alt="carted items thumbnail"
+          width={80}
+          height={80}
+        />
       </div>
-      <div className="table_body_item">{title}</div>
-      <div className="table_body_item">{prices.sale_price}</div>
-      <div className="table_body_item">1</div>
-      <div className="table_body_item">{prices.sale_price * 1}</div>
-      <div className="table_body_item">Remove</div>
+      <div className="table_body_item">
+        <h3 className="text-light text-black3">{title}</h3>
+      </div>
+      <div className="table_body_item">$ {prices.sale_price}</div>
+      <div className="table_body_item">
+        <div id="add_to_cart_area">
+          <button id="qty_controller" onClick={() => setQty(qty - 1)}>
+            -
+          </button>
+          <span id="cart_qty">{qty}</span>
+          <button id="qty_controller" onClick={() => setQty(qty + 1)}>
+            +
+          </button>
+        </div>
+      </div>
+      <div className="table_body_item">$ {prices.sale_price * 1}</div>
+      <div className="table_body_item">
+        <span className="bg-red-500 text-white cursor-pointer px-extra_padding3 rounded-full">
+          &times;
+        </span>
+      </div>
     </div>
   );
 }
