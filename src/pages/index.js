153b26a894d0be093b41e2e8905f8569cc52fa4a
MHRafix <fmhrafiz27@gmail.com>
2022-05-29 10:16:00 +0600

    code push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
+import Product from "../../models/Products";
 import LayoutContainer from "../components/commons/layout/LayoutContainer";
 import HomeMain from "../components/home_page/HomeMain";
+import db from "../utilities/database";
 
 export default function Home({ products }) {
   return (
@@ -14,21 +16,21 @@ export default function Home({ products }) {
   );
 }
 
-export async function getServerSideProps() {
-  const res = await fetch(`${process.env.ROOT_URI}/api/allproducts`);
-  const products = await res.json();
-
-  // Pass data to the page via props
-  return { props: { products } };
-}
-
 // export async function getServerSideProps() {
-//   await db.connect();
-//   const products = await Product.find({}).lean();
-//   await db.disconnect();
-//   return {
-//     props: {
-//       products: products.map(db.convertDocToObj),
-//     },
-//   };
+//   const res = await fetch(`${process.env.ROOT_URI}/api/allproducts`);
+//   const products = await res.json();
+
+//   // Pass data to the page via props
+//   return { props: { products } };
 // }
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find({}).lean();
+  await db.disconnect();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
+}
